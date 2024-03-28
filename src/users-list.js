93a83773fedf2c9eef4ca687f421ab06775928fc
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-03-27 22:51:55 -0400

    working on the user-table

--- src/users-list.js
+++ src/users-list.js
@@ -9,17 +9,17 @@ let Users = ({users, handleDelete,
         if (users) setIsLoading(false)
     return (
         <div id="users-list-frame">
+            <h2 className="user-list">Users</h2>
             <table id='user-table'>
                 <tbody>
 
-            <h2 className="user-list">Users</h2>
        {isLoading ? <h2>Loading...</h2> : users.map((user, index)=> {
            return (
                <tr key={user._id} id="the-row"
               
                >
 
-            <th className="username">{user.username}</th>
+            <th className="user-name">{user.username}</th>
             <td className='delete' onClick={()=> handleDelete(user._id)}>delete</td>
                 </tr>
     )
