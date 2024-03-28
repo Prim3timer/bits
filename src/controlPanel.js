99775ca83c6f0e7b7a70945a1a2d342f6da1aa5b
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-03-28 11:51:38 -0400

    working on navbar width

--- src/controlPanel.js
+++ src/controlPanel.js
@@ -4,10 +4,10 @@ let ControlPanel = ({changer}) => {
     return (
         <ul className='flexer'>
             <li 
-            id='first' onClick={()=>changer('createUser')}>Create Users</li>
+            id='first' onClick={()=>changer('createUser')}>Create User</li>
             <li 
             onClick={()=>changer('getUsers')}
-            >Users</li>
+            >Users Table</li>
             <li onClick={()=>changer('createExcercise')}
             >Create Excercise</li>
             <li onClick={()=>changer('getExcercises')}
