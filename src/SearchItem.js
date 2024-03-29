43e553df6a7d50b9fc06c2883e14415b6756c6f0
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-03-29 13:20:38 -0400

    search fiels

--- src/SearchItem.js
+++ src/SearchItem.js
@@ -1,8 +1,7 @@
 let SearchItem = ({search, setSearch})=> {
     return (
-        <form  className="search-form" onSubmit={(e)=> e.preventDefault()}>
+        <form  className="search-form"   onSubmit={(e)=> e.preventDefault()}>
             <input 
-            style={{padding: '0 .5rem', marginTop: '2rem'}}
             id="search"
             type="text"
             role="searchbox" 
